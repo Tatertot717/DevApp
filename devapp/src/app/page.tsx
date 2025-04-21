90452ed3a6588f271cc081b0c7b3e0efd8867c10
williampettit <14142910+williampettit@users.noreply.github.com>
2025-04-21 11:11:50 -0400

    change layout spacing + use button component

--- devapp/src/app/page.tsx
+++ devapp/src/app/page.tsx
@@ -1,26 +1,27 @@
 import Header from "@/components/header";
 import Link from "next/link";
 import { AnimatedQRLogo } from "@/components/animated-qr-code-logo";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white text-red-900">
       <Header />
 
-      <main className="flex flex-col items-center justify-center text-center p-10">
+      <main className="flex flex-col gap-8 items-center justify-center text-center p-10">
         <AnimatedQRLogo />
-        <h2 className="text-5xl font-bold mb-6">
-          UGA Class Check-In System
-        </h2>
-        <p className="text-xl max-w-2xl mb-10">
+        <h2 className="text-5xl font-bold">UGA Class Check-In System</h2>
+        <p className="text-xl max-w-2xl">
           Check in quickly and securely using dynamic QR codes.
         </p>
 
-        <Link href="/checkin/library">
-          <span className="bg-red-700 hover:bg-red-800 text-white px-6 py-3 rounded-md text-lg transition">
-            Go to Check-In
-          </span>
-        </Link>
+        <Button
+          className="bg-red-700 hover:bg-red-800 text-white px-6 py-3 rounded-md text-lg transition"
+          asChild
+          size="lg"
+        >
+          <Link href="/checkin/library">Go to Check-In</Link>
+        </Button>
       </main>
     </div>
   );
