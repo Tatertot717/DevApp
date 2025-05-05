1d95d04a886fbf43d773613f6f5ad56a6866dbcb
Rocco A <43506958+GenerelSchwerz@users.noreply.github.com>
2025-05-04 23:26:03 -0400

    good enough

--- devapp/jest.config.js
+++ devapp/jest.config.js
@@ -1,18 +1,22 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+
+  /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'node',      // ← use built-in jsdom
+    testEnvironment: 'node',
     testMatch: [
-      '<rootDir>/src/**/*.test.ts',
-      '<rootDir>/src/**/*.spec.ts'
-    ],
-    moduleFileExtensions: ['ts','tsx','js','jsx','json'],
+        '<rootDir>/src/**/*.test.ts',
+        '<rootDir>/src/**/*.spec.ts'
+      ],
     transform: {
-      '^.+\\.tsx?$': 'ts-jest'
+      '^.+\\.tsx?$': 'ts-jest',
+      '^.+\\.jsx?$': 'babel-jest',       // new: transform JS too
     },
-    moduleDirectories: ['node_modules','src'],
+    // transformIgnorePatterns: [
+    //   '/node_modules/(?!(?:@auth0/nextjs-auth0)/)'  // do transform auth0 pkg
+    // ],
     moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/src/$1'   // ← map "@/lib/..." → "src/lib/..."
+      '^@/(.*)$': '<rootDir>/src/$1',
+      '^@auth0/nextjs-auth0/server$': '<rootDir>/node_modules/@auth0/nextjs-auth0/dist/server/index.js'
     },
   };
   
